58da627b8dec1941d8c22250ee55ef1e01a025b8
yaroslavKulchenko <perekatipole123123@gmail.com>
2020-10-25 20:24:24 +0300

    prosto commit2

--- src/students.js
+++ src/students.js
@@ -1,67 +1,46 @@
-var app=new Vue({
-    el:"#app",
-    data:{
-        piece:"",
-        choose:"Yes",
-         nname:"",
-        nsurnname:"",
-        ngroup:"",
-        nriknarod:"",
-        nzdav:"",
-        students :[
-        {   
-            'surname':"#1 ",
-            'name': "#1",
-            'group': "RPZ 17 2/9",
-            'riknarod':"00 00 0000",
-            'zdav':true,
-            'id':0
-        },
-         {   
-            'surname':"#2 ",
-            'name': "#2 ",
-            'group': "RPZ 17 2/9",
-            'riknarod':"00 00 0000",
-            'zdav':false,
-            'id':1
-        },
-        {
-            'surname':"Terkin ",
-            'name': "Vasya",
-             'group': "RPZ 17 2/9",
-            'riknarod':"00 00 0000",
-            'zdav':false,
-            'id':2
-        },
-        {
-            'surname':"#3 ",
-            'name': "#3 ",
-            'group': "RPZ 17 2/9",
-            'riknarod':"00 00 0000",
-            'zdav':true,
-            'id':3
-        },
-    ],
-     arsort:[],
-    },
-    methods:{
-        del:function(id){
-            this.students.splice(id,1);
-            for (let i=0;i<this.students.length;i++){
-                if (this.students[i].id>id){
-                     this.students[i].id-=1;
-                }
-            }
-        },
-add:function(){
- this.students.push({
- 'surname':this.nsurnname,
- 'name': this.nnname,
- 'group': this.ngroup,
- 'riknarod':this.nriknarod,
- 'zdav':this.nzdav,
- 'id':this.students.length
-})
+var students = [{
+    "id" : 1,
+    "pib" : "Кульченко Ярослав",
+    "zdav" : true,
+    "group" : "RPZ 17 2/9",
+    "src": "https://orig00.deviantart.net/ee08/f/2009/073/e/d/free_red_panda_icon_100x100_by_supertuffpinkpuff.png"
+},
+{
+    "id" : 2,
+    "pib" : "Бабанський Ярослав",
+    "zdav" : false,
+    "group" : "RPZ 17 1/9",
+    "src": "https://cdn-learn.adafruit.com/assets/assets/000/012/878/thumb100/led_strips_doge.bmp?1386611464"
+},
+{
+    "id" : 3,
+    "pib" : "Щербань Ярослав",
+    "zdav" : true,
+    "group" : "RPZ 17 2/9",
+    "src": "https://vignette.wikia.nocookie.net/adventuretime/images/9/99/AT_Icons_100x100_Jake.jpg/revision/latest?cb=20120919222926&path-prefix=ar"
 }
-  }   
- })
+]
+
+new Vue({
+     el: '#app',
+     data: {
+         students: [],
+         search:'',
+         student: {"pib": "", "zdav": false, "group": ""}
+     },	 
+     mounted: function(){
+         this.students = students;
+     },
+     methods: {
+        deleteStudent(studId) {
+            this.students = this.students.filter(elem => {
+                return elem.id != studId;
+            });
+        },
+        addStudent() {
+            this.student.id = this.students.length+1;
+            this.students.push(JSON.parse(JSON.stringify(this.student)));
+        }
+     },
+  
+ });
